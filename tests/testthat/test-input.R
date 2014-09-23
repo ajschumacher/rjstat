a93a5227ca5cd9f7bec669b7d4b41c500ec323af
Håkon Malmedal <hmalmedal@gmail.com>
2014-09-23 18:04:06 +0200

    Tests

--- tests/testthat/test-input.R
+++ tests/testthat/test-input.R
@@ -4,40 +4,26 @@ oecd <- readLines("oecd-canada.json")
 non_unique <- data.frame(V1 = c("a", "a"), V2 = c("b", "b"), value = 1:2)
 
 test_that("wrong input fails", {
-    expect_that(fromJSONstat(1), throws_error("is not a character vector"))
-    expect_that(fromJSONstat(character(0)), throws_error("not greater than 0"))
-    expect_that(fromJSONstat(oecd, 1), throws_error("is not a string"))
-    expect_that(fromJSONstat(oecd, letters), throws_error("is not a string"))
-    expect_that(fromJSONstat(oecd, "a"),
-                throws_error("naming must be \"label\" or \"id\""))
-    expect_that(toJSONstat(1),
-                throws_error("(?:.*is not a data frame)(?:.* is not a list)"))
-    expect_that(toJSONstat(fromJSONstat(oecd), letters),
-                throws_error("is not a string"))
-    expect_that(toJSONstat(list(1)), throws_error("is not a data frame"))
-    expect_that(toJSONstat(non_unique),
-                throws_error("non-value columns must constitute a unique ID"))
-    expect_that(toJSONstat(data.frame(value = 1, id = 1)),
-                throws_error("not allowed column names"))
-    expect_that(toJSONstat(data.frame(value = 1, size = 1)),
-                throws_error("not allowed column names"))
-    expect_that(toJSONstat(data.frame(value = 1, role = 1)),
-                throws_error("not allowed column names"))
-})
-
-test_that("correct input doesn't fail", {
-    expect_that(fromJSONstat(oecd, naming = "label", use_factors = FALSE),
-                not(throws_error()))
-    expect_that(fromJSONstat(oecd, naming = "label", use_factors = TRUE),
-                not(throws_error()))
-    expect_that(fromJSONstat(oecd, naming = "id", use_factors = FALSE),
-                not(throws_error()))
-    expect_that(fromJSONstat(oecd, naming = "id", use_factors = TRUE),
-                not(throws_error()))
+    expect_error(fromJSONstat(1), "is not a character vector")
+    expect_error(fromJSONstat(character(0)), "not greater than 0")
+    expect_error(fromJSONstat(oecd, 1), "is not a string")
+    expect_error(fromJSONstat(oecd, letters), "is not a string")
+    expect_error(fromJSONstat(oecd, "a"), "naming must be \"label\" or \"id\"")
+    expect_error(toJSONstat(1), "(?:.*is not a data frame)(?:.* is not a list)")
+    expect_error(toJSONstat(fromJSONstat(oecd), letters), "is not a string")
+    expect_error(toJSONstat(list(1)), "is not a data frame")
+    expect_error(toJSONstat(non_unique),
+                 "non-value columns must constitute a unique ID")
+    expect_error(toJSONstat(data.frame(value = 1, id = 1)),
+                 "not allowed column names")
+    expect_error(toJSONstat(data.frame(value = 1, size = 1)),
+                 "not allowed column names")
+    expect_error(toJSONstat(data.frame(value = 1, role = 1)),
+                 "not allowed column names")
 })
 
 test_that("round-trip works", {
     df1 <- fromJSONstat(oecd, use_factors = TRUE)
     df2 <- fromJSONstat(toJSONstat(df1, digits = 8), use_factors = TRUE)
-    expect_that(df1, equals(df2))
+    expect_equal(df1, df2)
 })
--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -3,127 +3,145 @@ context("Output")
 salmon <- readLines("1120.json")
 
 test_that("dataset names are correct", {
-    expect_that(fromJSONstat(salmon, naming  = "label"),
-                has_names("Export of salmon, fish-farm bred, by commodity group, time and contents"))
-    expect_that(fromJSONstat(salmon, naming  = "id"), has_names("dataset"))
+    txt <- "Export of salmon, fish-farm bred, by commodity group, time and contents"
+    expect_named(fromJSONstat(salmon, naming  = "label"), txt)
+    expect_named(fromJSONstat(salmon, naming  = "id"), "dataset")
 })
 
 test_that("column names are correct", {
-    expect_that(fromJSONstat(salmon, naming  = "label")[[1]],
-                has_names(c("commodity group", "time", "contents", "value")))
-    expect_that(fromJSONstat(salmon, naming  = "id")[[1]],
-                has_names(c("VareGrupper2", "Tid", "ContentsCode", "value")))
+    expect_named(fromJSONstat(salmon, naming  = "label")[[1]],
+                 c("commodity group", "time", "contents", "value"))
+    expect_named(fromJSONstat(salmon, naming  = "id")[[1]],
+                 c("VareGrupper2", "Tid", "ContentsCode", "value"))
 })
 
 test_that("columns are correct", {
-    expect_that(fromJSONstat(salmon, naming  = "label")[[1]][[1]],
-                equals(c("Fish-farm bred salmon, fresh or chilled",
-                         "Fish-farm bred salmon, fresh or chilled",
-                         "Fish-farm bred salmon, fresh or chilled",
-                         "Fish-farm bred salmon, fresh or chilled",
-                         "Fish-farm bred salmon, frozen",
-                         "Fish-farm bred salmon, frozen",
-                         "Fish-farm bred salmon, frozen",
-                         "Fish-farm bred salmon, frozen")))
-    expect_that(fromJSONstat(salmon, naming  = "id")[[1]][[1]],
-                equals(c("01", "01", "01", "01", "02", "02", "02", "02")))
-    expect_that(fromJSONstat(salmon, naming  = "label")[[1]][[2]],
-                equals(c("2014U30", "2014U30", "2014U31", "2014U31", "2014U30",
-                         "2014U30", "2014U31", "2014U31")))
-    expect_that(fromJSONstat(salmon, naming  = "id")[[1]][[2]],
-                equals(c("2014U30", "2014U30", "2014U31", "2014U31", "2014U30",
-                         "2014U30", "2014U31", "2014U31")))
-    expect_that(fromJSONstat(salmon, naming  = "label")[[1]][[3]],
-                equals(c("Weight (tonnes)", "Price per kilo (NOK)",
-                         "Weight (tonnes)", "Price per kilo (NOK)",
-                         "Weight (tonnes)", "Price per kilo (NOK)",
-                         "Weight (tonnes)", "Price per kilo (NOK)")))
-    expect_that(fromJSONstat(salmon, naming  = "id")[[1]][[3]],
-                equals(c("Vekt", "Kilopris", "Vekt", "Kilopris", "Vekt",
-                         "Kilopris", "Vekt", "Kilopris")))
+    expect_equal(fromJSONstat(salmon, naming  = "label")[[1]][[1]],
+                 c("Fish-farm bred salmon, fresh or chilled",
+                   "Fish-farm bred salmon, fresh or chilled",
+                   "Fish-farm bred salmon, fresh or chilled",
+                   "Fish-farm bred salmon, fresh or chilled",
+                   "Fish-farm bred salmon, frozen",
+                   "Fish-farm bred salmon, frozen",
+                   "Fish-farm bred salmon, frozen",
+                   "Fish-farm bred salmon, frozen"))
+    expect_equal(fromJSONstat(salmon, naming  = "id")[[1]][[1]],
+                 c("01", "01", "01", "01", "02", "02", "02", "02"))
+    expect_equal(fromJSONstat(salmon, naming  = "label")[[1]][[2]],
+                 c("2014U30", "2014U30", "2014U31", "2014U31", "2014U30",
+                   "2014U30", "2014U31", "2014U31"))
+    expect_equal(fromJSONstat(salmon, naming  = "id")[[1]][[2]],
+                 c("2014U30", "2014U30", "2014U31", "2014U31", "2014U30",
+                   "2014U30", "2014U31", "2014U31"))
+    expect_equal(fromJSONstat(salmon, naming  = "label")[[1]][[3]],
+                 c("Weight (tonnes)", "Price per kilo (NOK)",
+                   "Weight (tonnes)", "Price per kilo (NOK)",
+                   "Weight (tonnes)", "Price per kilo (NOK)",
+                   "Weight (tonnes)", "Price per kilo (NOK)"))
+    expect_equal(fromJSONstat(salmon, naming  = "id")[[1]][[3]],
+                 c("Vekt", "Kilopris", "Vekt", "Kilopris", "Vekt",
+                   "Kilopris", "Vekt", "Kilopris"))
 })
 
 test_that("factors are factors", {
-    expect_that(fromJSONstat(salmon, naming  = "label",
-                             use_factors = TRUE)[[1]][[1]], is_a("factor"))
-    expect_that(fromJSONstat(salmon, naming  = "id",
-                             use_factors = TRUE)[[1]][[1]], is_a("factor"))
-    expect_that(fromJSONstat(salmon, naming  = "label",
-                             use_factors = TRUE)[[1]][[2]], is_a("factor"))
-    expect_that(fromJSONstat(salmon, naming  = "id",
-                             use_factors = TRUE)[[1]][[2]], is_a("factor"))
-    expect_that(fromJSONstat(salmon, naming  = "label",
-                             use_factors = TRUE)[[1]][[3]], is_a("factor"))
-    expect_that(fromJSONstat(salmon, naming  = "id",
-                             use_factors = TRUE)[[1]][[3]], is_a("factor"))
+    expect_is(fromJSONstat(salmon, naming  = "label",
+                           use_factors = TRUE)[[1]][[1]],
+              "factor")
+    expect_is(fromJSONstat(salmon, naming  = "id",
+                           use_factors = TRUE)[[1]][[1]],
+              "factor")
+    expect_is(fromJSONstat(salmon, naming  = "label",
+                           use_factors = TRUE)[[1]][[2]],
+              "factor")
+    expect_is(fromJSONstat(salmon, naming  = "id",
+                           use_factors = TRUE)[[1]][[2]],
+              "factor")
+    expect_is(fromJSONstat(salmon, naming  = "label",
+                           use_factors = TRUE)[[1]][[3]],
+              "factor")
+    expect_is(fromJSONstat(salmon, naming  = "id",
+                           use_factors = TRUE)[[1]][[3]],
+              "factor")
 })
 
 test_that("factor levels are correct", {
-    expect_that(nlevels(fromJSONstat(salmon, naming  = "label",
-                                     use_factors = TRUE)[[1]][[1]]), equals(2))
-    expect_that(levels(fromJSONstat(salmon, naming  = "label",
+    expect_equal(nlevels(fromJSONstat(salmon, naming  = "label",
+                                      use_factors = TRUE)[[1]][[1]]),
+                 2)
+    expect_equal(levels(fromJSONstat(salmon, naming  = "label",
                                      use_factors = TRUE)[[1]][[1]]),
-                equals(c("Fish-farm bred salmon, fresh or chilled",
-                         "Fish-farm bred salmon, frozen")))
-    expect_that(nlevels(fromJSONstat(salmon, naming  = "id",
-                                     use_factors = TRUE)[[1]][[1]]), equals(2))
-    expect_that(levels(fromJSONstat(salmon, naming  = "id",
-                                    use_factors = TRUE)[[1]][[1]]),
-                equals(c("01", "02")))
-    expect_that(nlevels(fromJSONstat(salmon, naming  = "label",
-                                     use_factors = TRUE)[[1]][[2]]), equals(2))
-    expect_that(levels(fromJSONstat(salmon, naming  = "label",
-                                    use_factors = TRUE)[[1]][[2]]),
-                equals(c("2014U30", "2014U31")))
-    expect_that(nlevels(fromJSONstat(salmon, naming  = "id",
-                                     use_factors = TRUE)[[1]][[2]]), equals(2))
-    expect_that(levels(fromJSONstat(salmon, naming  = "id",
-                                    use_factors = TRUE)[[1]][[2]]),
-                equals(c("2014U30", "2014U31")))
-    expect_that(nlevels(fromJSONstat(salmon, naming  = "label",
-                                     use_factors = TRUE)[[1]][[3]]), equals(2))
-    expect_that(levels(fromJSONstat(salmon, naming  = "label",
-                                    use_factors = TRUE)[[1]][[3]]),
-                equals(c("Weight (tonnes)", "Price per kilo (NOK)")))
-    expect_that(nlevels(fromJSONstat(salmon, naming  = "id",
-                                     use_factors = TRUE)[[1]][[3]]), equals(2))
-    expect_that(levels(fromJSONstat(salmon, naming  = "id",
-                                    use_factors = TRUE)[[1]][[3]]),
-                equals(c("Vekt", "Kilopris")))
+                 c("Fish-farm bred salmon, fresh or chilled",
+                   "Fish-farm bred salmon, frozen"))
+    expect_equal(nlevels(fromJSONstat(salmon, naming  = "id",
+                                      use_factors = TRUE)[[1]][[1]]),
+                 2)
+    expect_equal(levels(fromJSONstat(salmon, naming  = "id",
+                                     use_factors = TRUE)[[1]][[1]]),
+                 c("01", "02"))
+    expect_equal(nlevels(fromJSONstat(salmon, naming  = "label",
+                                      use_factors = TRUE)[[1]][[2]]),
+                 2)
+    expect_equal(levels(fromJSONstat(salmon, naming  = "label",
+                                     use_factors = TRUE)[[1]][[2]]),
+                 c("2014U30", "2014U31"))
+    expect_equal(nlevels(fromJSONstat(salmon, naming  = "id",
+                                      use_factors = TRUE)[[1]][[2]]),
+                 2)
+    expect_equal(levels(fromJSONstat(salmon, naming  = "id",
+                                     use_factors = TRUE)[[1]][[2]]),
+                 c("2014U30", "2014U31"))
+    expect_equal(nlevels(fromJSONstat(salmon, naming  = "label",
+                                      use_factors = TRUE)[[1]][[3]]),
+                 2)
+    expect_equal(levels(fromJSONstat(salmon, naming  = "label",
+                                     use_factors = TRUE)[[1]][[3]]),
+                 c("Weight (tonnes)", "Price per kilo (NOK)"))
+    expect_equal(nlevels(fromJSONstat(salmon, naming  = "id",
+                                      use_factors = TRUE)[[1]][[3]]),
+                 2)
+    expect_equal(levels(fromJSONstat(salmon, naming  = "id",
+                                     use_factors = TRUE)[[1]][[3]]),
+                 c("Vekt", "Kilopris"))
 })
 
 test_that("factor integer codes are correct", {
-    expect_that(unclass(fromJSONstat(salmon, use_factors = TRUE)[[1]][[1]]),
-                is_equivalent_to(c(1, 1, 1, 1, 2, 2, 2, 2)))
-    expect_that(unclass(fromJSONstat(salmon, use_factors = TRUE)[[1]][[2]]),
-                is_equivalent_to(c(1, 1, 2, 2, 1, 1, 2, 2)))
-    expect_that(unclass(fromJSONstat(salmon, use_factors = TRUE)[[1]][[3]]),
-                is_equivalent_to(c(1, 2, 1, 2, 1, 2, 1, 2)))
+    expect_equivalent(unclass(fromJSONstat(salmon,
+                                           use_factors = TRUE)[[1]][[1]]),
+                      c(1, 1, 1, 1, 2, 2, 2, 2))
+    expect_equivalent(unclass(fromJSONstat(salmon,
+                                           use_factors = TRUE)[[1]][[2]]),
+                      c(1, 1, 2, 2, 1, 1, 2, 2))
+    expect_equivalent(unclass(fromJSONstat(salmon,
+                                           use_factors = TRUE)[[1]][[3]]),
+                      c(1, 2, 1, 2, 1, 2, 1, 2))
 })
 
 test_that("values are correct", {
-    expect_that(fromJSONstat(salmon)[[1]]$value,
-                equals(c(14297, 42.23, 15439, 37.7, 342, 43.49, 447, 43.32)))
+    expect_equal(fromJSONstat(salmon)[[1]]$value,
+                 c(14297, 42.23, 15439, 37.7, 342, 43.49, 447, 43.32))
 })
 
 test_that("attributes are correct", {
-    expect_that(attr(fromJSONstat(salmon)[[1]], "source"),
-                equals("Statistics Norway"))
-    expect_that(attr(fromJSONstat(salmon)[[1]], "updated"),
-                equals("2014-08-09T15:21:14Z"))
+    expect_equal(attr(fromJSONstat(salmon)[[1]], "source"),
+                 "Statistics Norway")
+    expect_equal(attr(fromJSONstat(salmon)[[1]], "updated"),
+                 "2014-08-09T15:21:14Z")
 })
 
 test_that("single-dimension input gives correct output", {
-    expect_that(toJSONstat(data.frame(V1 = "a", value = 1)),
-                is_identical_to(structure("{\"dataset\":{\"dimension\":{\"V1\":{\"category\":{\"index\":[\"a\"]}},\"id\":[\"V1\"],\"size\":[1]},\"value\":[1]}}",
-                                          class = "json")))
+    txt <- "{\"dataset\":{\"dimension\":{\"V1\":{\"category\":{\"index\":[\"a\"]}},\"id\":[\"V1\"],\"size\":[1]},\"value\":[1]}}"
+    expect_identical(toJSONstat(data.frame(V1 = "a", value = 1)),
+                     structure(txt, class = "json"))
 })
 
 test_that("sparse cubes give correct output", {
-    expect_that(toJSONstat(data.frame(V1 = c("a", "b"), V2 = c("A", "B"),
-                                      value = 1:2)),
-                is_identical_to(structure("{\"dataset\":{\"dimension\":{\"V1\":{\"category\":{\"index\":[\"a\",\"b\"]}},\"V2\":{\"category\":{\"index\":[\"A\",\"B\"]}},\"id\":[\"V1\",\"V2\"],\"size\":[2,2]},\"value\":{\"0\":1,\"3\":2}}}",
-                                          class = "json")))
-    expect_that(fromJSONstat("{\"dataset\":{\"dimension\":{\"V1\":{\"category\":{\"index\":[\"a\",\"b\"]}},\"V2\":{\"category\":{\"index\":[\"A\",\"B\"]}},\"id\":[\"V1\",\"V2\"],\"size\":[2,2]},\"value\":{\"0\":1,\"3\":2}}}"),
-                is_identical_to(structure(list(dataset = structure(list(V1 = c("a", "a", "b", "b"), V2 = c("A", "B", "A", "B"), value = c(1, NA, NA, 2)), .Names = c("V1", "V2", "value"), class = "data.frame", row.names = c(NA, -4L))), .Names = "dataset")))
+    txt <- "{\"dataset\":{\"dimension\":{\"V1\":{\"category\":{\"index\":[\"a\",\"b\"]}},\"V2\":{\"category\":{\"index\":[\"A\",\"B\"]}},\"id\":[\"V1\",\"V2\"],\"size\":[2,2]},\"value\":{\"0\":1,\"3\":2}}}"
+    expect_identical(toJSONstat(data.frame(V1 = c("a", "b"), V2 = c("A", "B"),
+                                           value = 1:2)),
+                     structure(txt, class = "json"))
+    expect_identical(fromJSONstat(txt),
+                     list(dataset = data.frame(V1 = c("a", "a", "b", "b"),
+                                               V2 = c("A", "B", "A", "B"),
+                                               value = c(1, NA, NA, 2),
+                                               stringsAsFactors = FALSE)))
 })
