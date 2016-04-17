cfc2e884db1472991abac72c838042eb6bdf93cb
Håkon Malmedal <hmalmedal@gmail.com>
2016-04-17 22:05:39 +0200

    Tests for collection responses

--- tests/testthat/test-classes.R
+++ tests/testthat/test-classes.R
@@ -10,3 +10,17 @@ test_that("dataset responses work", {
         getElement("value") %>%
         expect_equal(1)
 })
+
+test_that("collection responses work", {
+    fromJSONstat("collection.json") %>%
+        expect_type("list") %>%
+        expect_named("A collection") %>%
+        getElement(1) %>%
+        expect_type("list") %>%
+        expect_named("A dataset") %>%
+        getElement(1) %>%
+        expect_s3_class("data.frame") %>%
+        expect_named(c("A dimension", "value")) %>%
+        getElement("value") %>%
+        expect_equal(1)
+})
