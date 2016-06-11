6c1ec4588ec665cc98a201eeb7b2ad1471df308e
Måns Magnusson <mons.magnusson@gmail.com>
2016-06-11 07:45:01 +0200

    Fixed test suite

--- tests/testthat/test-constructor.R
+++ tests/testthat/test-constructor.R
@@ -1,22 +1,22 @@
 context("Constructor")
 
 test_that("constructor works", {
-    jsonstat("collection.json") %>%
+    as.jsonstat("collection.json") %>%
         expect_class("json-stat") %>%
         expect_class("collection")
-    jsonstat("dataset.json") %>%
+    as.jsonstat("dataset.json") %>%
         expect_class("json-stat") %>%
         expect_class("dataset")
-    expect_error(jsonstat("notjsonstat.json"))
+    expect_error(as.jsonstat("notjsonstat.json"))
 
-    expect_silent(jsonstat("oecd.json"))
-    expect_silent(jsonstat("canada.json"))
-    expect_silent(jsonstat("oecd-canada-col.json"))
-    expect_silent(jsonstat("galicia.json"))
-    expect_silent(jsonstat("order.json"))
-    expect_silent(jsonstat("hierarchy.json"))
-    expect_silent(jsonstat("us-gsp.json"))
-    expect_silent(jsonstat("us-unr.json"))
-    expect_silent(jsonstat("us-labor.json"))
-    expect_silent(jsonstat("collection_sample.json"))
+    expect_silent(as.jsonstat("oecd.json"))
+    expect_silent(as.jsonstat("canada.json"))
+    expect_silent(as.jsonstat("oecd-canada-col.json"))
+    expect_silent(as.jsonstat("galicia.json"))
+    expect_silent(as.jsonstat("order.json"))
+    expect_silent(as.jsonstat("hierarchy.json"))
+    expect_silent(as.jsonstat("us-gsp.json"))
+    expect_silent(as.jsonstat("us-unr.json"))
+    expect_silent(as.jsonstat("us-labor.json"))
+    expect_silent(as.jsonstat("collection_sample.json"))
 })
