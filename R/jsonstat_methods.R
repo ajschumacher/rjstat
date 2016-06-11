3ce5c6dae90e183df2ad1dbaf7ee8641e4f1d816
Måns Magnusson <mons.magnusson@gmail.com>
2016-06-11 16:50:17 +0200

    Added dim and dimnames methods

--- R/jsonstat_methods.R
+++ R/jsonstat_methods.R
@@ -7,9 +7,23 @@ print.jsonstat <- function(x, ...){
 print.jsonstat_dataset <- function(x, ...){
     NextMethod(x)
     if(!is.null(x$label)) cat("\n", x$label, sep="")
-    cat("\nsize: ")
+    cat("\ndimensions: ")
     cat(paste(x$id, "(", x$size, ")", sep=""), sep = ", ")
 }
 
+#' @export
+dim.jsonstat_dataset <- function(x){
+    x$size
+}
 
+#' @export
+dimnames.jsonstat_dataset <- function(x){
+    dn <- lapply(x$dimension,
+                 FUN=function(X) {
+                     idx <- names(X$category$index)
+                     if(is.null(idx)) idx <- names(X$category$label)
+                     idx
+                     })
+    dn[x$id]
+}
 
--- tests/testthat/test-jsonstat_methods.R
+++ tests/testthat/test-jsonstat_methods.R
@@ -4,3 +4,26 @@ test_that("print works", {
     expect_output(print(as.jsonstat("dataset.json")), regexp = "JSON-stat dataset")
     expect_output(print(as.jsonstat("collection.json")), regexp = "JSON-stat collection")
 })
+
+test_that("dim works", {
+    dim(as.jsonstat("oecd.json")) %>%
+        expect_equal(c(1, 36, 12))
+    dim(as.jsonstat("canada.json")) %>%
+        expect_equal(c(1, 1, 20, 2, 3))
+    dim(as.jsonstat("galicia.json")) %>%
+        expect_equal(c(6, 22, 3, 2, 5, 1))
+    dim(as.jsonstat("collection.json")) %>%
+        expect_null()
+})
+
+
+test_that("dimnames works", {
+    test_dimnames_canada <- list(country = "CA")
+    test_dimnames_canada$year <- "2012"
+    test_dimnames_canada$age <- c("T", "4", "9", "14", "19", "24", "29", "34", "39", "44", "49", "54", "59", "64", "69", "74", "79", "84", "89", "older")
+    test_dimnames_canada$concept <- c("POP", "PERCENT")
+    test_dimnames_canada$sex <- c("T", "M", "F")
+
+    dimnames(as.jsonstat("canada.json")) %>%
+        expect_equal(test_dimnames_canada)
+})
