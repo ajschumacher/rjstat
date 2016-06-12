10d5d722072fa50f47b3e7814a1b462cfa2bf5e5
Måns Magnusson <mons.magnusson@gmail.com>
2016-06-12 17:03:11 +0200

    Added status accessor

--- R/jsonstat_accessors.R
+++ R/jsonstat_accessors.R
@@ -23,3 +23,41 @@ id <- function(x){
     x
 }
 
+
+#' \code{status} accessors of \code{jsonstat} objects
+#'
+#' @description
+#' Access and change \code{status}s of jsonstat objects.
+#'
+#' @param x a \code{jsonstat_dataset} object
+#' @param value a character vector of up to the same length as \code{as.vector(x)} or a named vector with index as vector names.
+#'
+#' @export
+status <- function(x){
+    assert_class(x, "jsonstat_dataset")
+    x$status
+}
+value <- "e"
+names(value) <- "93"
+#' @rdname status
+#' @export
+`status<-` <- function(x, value){
+    assert_class(x, "jsonstat_dataset")
+    max_len <- length(as.vector(x))
+    assert_character(value, max.len = max_len)
+    if(is.null(names(value))){
+        if(length(value) == 1) {
+            x$status <- value
+        } else if(length(value) == max_len) {
+            x$status <- value
+        } else {
+            stop()
+        }
+    } else {
+        assert_subset(names(value), as.character(1:max_len))
+        x$status <- value
+    }
+    x
+}
+
+
--- tests/testthat/test-jsonstat_accessors.R
+++ tests/testthat/test-jsonstat_accessors.R
@@ -8,3 +8,18 @@ test_that("id", {
     expect_equal(id(x),  c("1", "2", "3"))
 })
 
+test_that("status", {
+    x <- as.jsonstat("oecd.json")
+    expect_equal(status(x)[[1]], "e")
+    expect_equal(names(status(x))[1], "10")
+    expect_error(status(x) <- 5)
+    expect_error(status(x) <- c("E", "D"))
+    expect_silent(status(x) <- "e")
+    expect_equal(status(x), "e")
+    vals <- c("e", "g", "f")
+    names(vals) <- c("32", "2", "17")
+    expect_silent(status(x) <- vals)
+    expect_equal(status(x), vals)
+    expect_silent(status(x) <- rep("e", length(as.vector(x))))
+})
+
